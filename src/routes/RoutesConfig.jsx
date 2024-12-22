bf72817576d39bbe6add54d22c0d9816fddec3b4
unknown <ranem.elnoury2003@gmail.com>
2024-12-23 01:53:55 +0200

    message

--- src/routes/RoutesConfig.jsx
+++ src/routes/RoutesConfig.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Tour from "../pages/Tour/Tour";
 import HotelDetail from "../pages/Details/HotelDetails";
+import TripsDetailsPage from "../pages/TripsDetailsPage/TripsDetailsPage";
 
 function RoutesConfig() {
   return (
       <Routes>
         <Route path="/Tour" element={<Tour />} />
         <Route path="/HotelDetails" element={<HotelDetail/>}/>
+        <Route path="/TripsDetailsPage" element={<TripsDetailsPage/>}/>
       </Routes>
   );
 }
